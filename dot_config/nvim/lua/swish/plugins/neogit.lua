d8760c61177092ee2541251549543f1b6ea48a42
Doug Swisher <big.swish@gmail.com>
2025-12-14 16:29:51 -0600

    Disable neogit

--- dot_config/nvim/lua/swish/plugins/neogit.lua
+++ dot_config/nvim/lua/swish/plugins/neogit.lua
@@ -1,7 +1,24 @@
 
 -- See https://github.com/NeogitOrg/neogit
 -- TODO: install and set up neogit
+-- I tried it out, and it is kinda slow. For now at least, I'll stick with tig in another window.
 
 return {
+  -- "NeogitOrg/neogit",
+  -- lazy = true,
+  -- dependencies = {
+  --   "nvim-lua/plenary.nvim",         -- required
+  --   -- "sindrets/diffview.nvim",        -- optional - Diff integration
+  --
+  --   -- Only one of these is needed.
+  --   "nvim-telescope/telescope.nvim", -- optional
+  --   -- "ibhagwan/fzf-lua",              -- optional
+  --   -- "nvim-mini/mini.pick",           -- optional
+  --   -- "folke/snacks.nvim",             -- optional
+  -- },
+  -- cmd = "Neogit",
+  -- keys = {
+  --   { "<leader>gg", "<cmd>Neogit<cr>", desc = "neogit: Show UI" }
+  -- }
 }
 
