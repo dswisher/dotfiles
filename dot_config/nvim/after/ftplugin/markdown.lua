0ddb8bf8790970877b41b5d75c5602415608e757
Doug Swisher <big.swish@gmail.com>
2026-04-14 08:29:32 -0500

    Tweak conceallevel in markdown files to stop hiding things

--- dot_config/nvim/after/ftplugin/markdown.lua
+++ dot_config/nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,9 @@
 
-vim.opt_local.conceallevel = 2
+-- Set the conceallevel, which "hides" bits of markdown, like back-ticks.
+--   0 = Concealed text is shown normally — nothing hidden
+--   1 = Each concealed region is replaced with one character (or a space)
+--   2 = Concealed text is completely hidden unless the cursor is on that line
+--   3 = Concealed text is always hidden, even when the cursor is on that line
+-- I had this set to 2 for a while, and hated it.
+vim.opt_local.conceallevel = 0
 
